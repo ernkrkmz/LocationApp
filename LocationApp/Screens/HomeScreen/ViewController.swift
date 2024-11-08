9d399fa7549d7ecdcadd0007a205bdd656ae441b
Ernkrkmz <eren_korkmaz_@hotmail.com>
2024-11-09 01:14:02 +0300

    Create user eklendi

--- LocationApp/Screens/HomeScreen/ViewController.swift
+++ LocationApp/Screens/HomeScreen/ViewController.swift
@@ -24,20 +24,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(Auth.auth().currentUser?.email)
+        
         
     }
 
     @IBAction func btnLoginClicked(_ sender: Any) {
+
         if txtEmail.text != "" && txtPassword.text != "" {
             Auth.auth().signIn(withEmail: "deneme@gmail.com", password: "12345") { result, error in
                 if let error {
-                    print(error.localizedDescription)
                     Alert().showAlert(title: "Error", message: error.localizedDescription)
-                } else {
-                    print("Login Success")
-                    self.performSegue(withIdentifier: "toTabbar", sender: nil)
                 }
+                else { self.performSegue(withIdentifier: "toTabbar", sender: nil) }
             }
         }else {
             Alert().showAlert(title: "Empty Fields", message: "Please fill in all fields")
--- LocationApp/Screens/RegisterScreen/RegisterVC.swift
+++ LocationApp/Screens/RegisterScreen/RegisterVC.swift
@@ -6,24 +6,42 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class RegisterVC: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var txtConfirmPass: UITextField!
+    
+    @IBOutlet weak var btnRegister: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func btnRegisterClicked(_ sender: Any) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //    TODO: succes uyarisini göstermiyor
+        if txtEmail.text != "" && txtPassword.text != "" && txtConfirmPass.text != "" {
+            Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { result, error in
+                if let error {
+                    Alert().showAlert(title: "Error", message: error.localizedDescription)
+                } else {
+                    Alert().showAlert(title: "Success", message: "User created successfully")
+                    self.dismiss(animated: true)
+                }
+            }
+            
+            
+        } else {
+            Alert().showAlert(title: "Error", message: "Please fill all fields")
+        }
+        
     }
-    */
+    
 
 }
