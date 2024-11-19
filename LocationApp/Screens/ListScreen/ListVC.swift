595b798a5fba6fa7f2ca9a345e0de86817cb814a
Ernkrkmz <eren_korkmaz_@hotmail.com>
2024-11-19 17:53:20 +0300

    listScreen oluşturuldu

---
+++ LocationApp/Screens/ListScreen/ListVC.swift
@@ -0,0 +1,21 @@
+//
+//  ListVC.swift
+//  LocationApp
+//
+//  Created by Eren Korkmaz on 19.11.2024.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- LocationApp/Screens/LocationDetailScreen/LocationDetailVC.swift
+++ LocationApp/Screens/LocationDetailScreen/LocationDetailVC.swift
@@ -6,24 +6,77 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import SwiftAlertView
 
 class LocationDetailVC: UIViewController {
 
+    
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtComment: UITextField!
+    
+    @IBOutlet weak var btnAddPhoto: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var btnSave: UIButton!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var LocationManager = CLLocationManager()
+    var selectedLongitude = ""
+    var selectedLatitude = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupMap()
+        addGestureRecognizer()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnAddPhotoClicked(_ sender: Any) {
     }
-    */
-
+    @IBAction func btnSaveClicked(_ sender: Any) {
+    }
+    
 }
+
+extension LocationDetailVC: MKMapViewDelegate , CLLocationManagerDelegate{
+    func setupMap() {
+        self.mapView.delegate = self
+        self.LocationManager.delegate = self
+        
+        self.LocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.LocationManager.requestWhenInUseAuthorization()
+        self.LocationManager.startUpdatingLocation()
+        
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    func addGestureRecognizer() {
+        let getGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gesture: )))
+        getGestureRecognizer.minimumPressDuration = 2
+        self.mapView.addGestureRecognizer(getGestureRecognizer)
+    }
+    @objc func chooseLocation(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let location = gesture.location(in: self.mapView)
+            let coordinate = self.mapView.convert(location, toCoordinateFrom: self.mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = ""
+            annotation.subtitle = ""
+            
+            self.selectedLatitude = String(coordinate.latitude)
+            self.selectedLongitude = String(coordinate.longitude)
+            
+            self.mapView.addAnnotation(annotation)
+        }
+    }}
